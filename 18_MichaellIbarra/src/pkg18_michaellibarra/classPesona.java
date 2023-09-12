696a4b8aa3d886fa391e735b1aa331f2b12f2f1e
Michaell Ibarra <131507781+MichaellIbarra@users.noreply.github.com>
2023-09-12 09:10:07 -0500

    entrega 1

--- 18_MichaellIbarra/src/pkg18_michaellibarra/classPesona.java
+++ 18_MichaellIbarra/src/pkg18_michaellibarra/classPesona.java
@@ -10,4 +10,34 @@ package pkg18_michaellibarra;
  */
 public class classPesona {
     
+    //atributos
+    String nombre, apellidos, lugardeNacimiento, ocupacion;
+    int edad;
+    
+    //metodos
+    public void saludar(){
+        System.out.println("Hola, mi nombre es "+ nombre + apellidos + "tengo de edad " + edad + " vivo en "+ lugardeNacimiento + " mi ocupación es" + ocupacion);
+    }
+    
+    //sobrecarga de metodo
+    
+     public void saludar(String nombre) {
+        System.out.println("Hola, " + nombre + "!");
+    }
+
+    public void saludar(String nombre, String apellido) {
+        System.out.println("Hola, " + nombre + " " + apellidos + "!");
+    }
+    
+    //retorno
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+      
 }
