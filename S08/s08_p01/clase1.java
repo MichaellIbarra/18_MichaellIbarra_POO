cea4c7d515216d775bb64569bf32dd2120d91346
Michaell Ibarra <131507781+MichaellIbarra@users.noreply.github.com>
2023-09-15 09:23:04 -0500

    complket

---
+++ S08/s08_p01/clase1.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package s08_p01;
+
+/**
+ *
+ * @author Lab05
+ */
+public class clase1 {
+    
+}
---
+++ S08/s08_p02/clase2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package s08_p02;
+
+/**
+ *
+ * @author Lab05
+ */
+public class clase2 {
+    public String a,b;
+    private String c,d;
+    
+    public void saludar(){
+        System.out.println("Hola"+ a+ " mi apellido es " + b);
+    }
+    
+     private void saludar2(){
+        System.out.println("Hola"+ c + " mi apellido es " + d);
+    }
+}
