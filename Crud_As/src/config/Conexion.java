671f80cdc0353a6e9fa4ef2491d9d016662d01ea
MichaellIbarra <michaell.ibarra@vallegrande.edu.pe>
2023-11-11 18:44:34 -0500

    crud

---
+++ Crud_As/src/config/Conexion.java
@@ -0,0 +1,22 @@
+package config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    Connection con;
+
+    public Conexion() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/crud?characterEncoding=UTF-8", "root", "admin");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Error conexion " + e);
+        }
+    }
+
+    public Connection getConnection() {
+        return con;
+    }
+}
